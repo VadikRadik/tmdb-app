57b5c800df95ec79d4f4e26ece5030fed098c4c0
Xardas <vadikradik@yandex.ru>
2022-12-19 19:48:04 +0300

    mobile styles fixed fetch data added

--- src/components/movie-app/movie-app.js
+++ src/components/movie-app/movie-app.js
@@ -359,7 +359,46 @@ export default class MovieApp extends React.Component {
     },
   ]
 
+  state = {
+    movies: null,
+    resultsCount: null,
+    resultPage: null,
+  }
+
+  async getMovies(keyWords) {
+    const res = await fetch(
+      // eslint-disable-next-line no-undef
+      `https://api.themoviedb.org/3/search/movie?query=${keyWords}&api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+    )
+    if (!res.ok) {
+      throw new Error(`Unable to fetch movies, responce status: ${res.status}`)
+    }
+
+    const result = await res.json().catch((err) => console.log(err))
+
+    return result
+  }
+
+  updatePage = (keyWords) => {
+    this.getMovies(keyWords).then((result) => {
+      console.log(result)
+      this.setState(() => {
+        return {
+          movies: result.results,
+          resultsCount: result.total_results,
+          resultPage: result.page,
+        }
+      })
+    })
+  }
+
+  constructor() {
+    super()
+    this.updatePage('returns')
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="app body__app">
         <div className="app__tabs-switch">
@@ -381,8 +420,8 @@ export default class MovieApp extends React.Component {
           />
         </div>
         <Input placeholder="Type to search..." />
-        <MovieCardList movies={this.cardsData} />
-        <Pagination defaultCurrent={1} total={50} className="app__pagination" />
+        <MovieCardList movies={this.state.movies} />
+        <Pagination defaultCurrent={1} total={this.state.resultsCount} className="app__pagination" />
       </div>
     )
   }
--- src/components/movie-card-list/movie-card-list.js
+++ src/components/movie-card-list/movie-card-list.js
@@ -5,26 +5,34 @@ import MovieCard from '../movie-card'
 
 import './movie-card-list.css'
 
-const MovieCardList = ({ movies }) => {
-  const isMobile = window.matchMedia('only screen and (max-width: 768px)').matches
-  const gridGutters = isMobile ? [16, 20] : [36, 36]
-  const cards = movies.map((movie) => {
-    return (
-      <MovieCard
-        key={movie.id}
-        poster={movie.poster_path}
-        title={movie.title}
-        rate={movie.vote_average}
-        date={movie.release_date}
-        overview={movie.overview}
-      />
-    )
-  })
-  return (
-    <div className="movie-card-list app__card-list">
-      <Row gutter={gridGutters}>{cards}</Row>
-    </div>
-  )
-}
+export default class MovieCardList extends React.Component {
+  static defaultProps = {
+    movies: [],
+  }
 
-export default MovieCardList
+  render() {
+    if (this.props.movies === null) {
+      return null
+    }
+    console.log(this.props.movies)
+    const isMobile = window.matchMedia('only screen and (max-width: 768px)').matches
+    const gridGutters = isMobile ? [16, 20] : [36, 36]
+    const cards = this.props.movies.map((movie) => {
+      return (
+        <MovieCard
+          key={movie.id}
+          poster={movie.poster_path}
+          title={movie.title}
+          rate={movie.vote_average}
+          date={movie.release_date}
+          overview={movie.overview}
+        />
+      )
+    })
+    return (
+      <div className="movie-card-list app__card-list">
+        <Row gutter={gridGutters}>{cards}</Row>
+      </div>
+    )
+  }
+}
